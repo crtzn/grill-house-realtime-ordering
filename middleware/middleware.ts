2312a54157115acce45b0c043608b6df1acc6af0
Cortezano <christianjakecortezano@gmail.com>
2024-09-24 03:10:24 +0800

    deleted some files

--- middleware/middleware.ts
+++
@@ -1,4 +0,0 @@
-// so gumamit tayo ng middleware, para ma prevent to enter everyone.
-// we gonna use create middleware using auth
-
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
