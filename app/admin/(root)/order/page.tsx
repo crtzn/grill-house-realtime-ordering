0f511934051acc1b91eee154b132f833da67357b
Cortezano <christianjakecortezano@gmail.com>
2024-09-27 07:53:34 +0800

    admin: fetching order from customer

--- app/admin/(root)/order/page.tsx
+++ app/admin/(root)/order/page.tsx
@@ -1,25 +1,22 @@
+"use client";
+
 import React from "react";
 import Header from "@/components/HeaderBox";
-import { CirclePlus } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import OrderTable from "@/components/OrderTable";
-import { NewOrderBtn } from "@/components/order/Modal";
 
-interface OrderPackage {
-  clasica: number;
-  Suprema: number;
-  UCM: number; //Unlimited Combo Meals
-  SC: number; //Supreme Combo
-}
-
-export default function page() {
+const OrderPage: React.FC = () => {
   return (
     <div className="flex flex-col gap-5 w-full">
       <div className="flex justify-between">
         <Header title="Order" />
-        <NewOrderBtn />
       </div>
       <OrderTable />
     </div>
   );
+};
+
+export default OrderPage;
+
+{
+  /* <NewOrderBtn /> */
 }
