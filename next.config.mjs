c303750b622e848906941a81995c7467193da44a
“Zuaren““ <christianjakecortezano@gmail.com>
2024-11-20 02:02:14 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["lbjyvkxjnqiydfivnaog.supabase.co"], // Add your Supabase hostname here
+  },
+};
 
 export default nextConfig;
