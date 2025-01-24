a6d6bea13d04ec91e171cf50bf2449c8ec8bb628
Zuaren <christianjakecortezano@gmail.com>
2025-01-24 14:29:41 +0800

    test commit if may changes ba sa vercel

--- app/customer/error/page.tsx
+++ app/customer/error/page.tsx
@@ -2,7 +2,7 @@
 export default function ErrorPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-2xl font-bold text-red-600 mb-4">Error Occurred</h1>
+      <h1 className="text-2xl font-bold text-red-600 mb-4">testing commit</h1>
       <p className="text-gray-600">
         Sorry, something went wrong while processing your request.
       </p>
