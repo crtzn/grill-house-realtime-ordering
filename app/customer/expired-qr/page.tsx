fa012875c63c1ae39b006913cb656c2c7e40602d
Zuaren <christianjakecortezano@gmail.com>
2025-02-11 18:49:18 +0800

    update text for expired-qr

--- app/customer/expired-qr/page.tsx
+++ app/customer/expired-qr/page.tsx
@@ -5,14 +5,12 @@ import Logo from "@/public/assets/Logo.png";
 
 export default function ExpiredQRPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="text-center">
-        <Image src={Logo} alt="logo" width={500} height={500}></Image>
-        <h1 className="text-4xl font-bold mb-4">QR Code Expired</h1>
-        <p className="text-xl mb-8">
-          This QR code has expired or is no longer valid.
-        </p>
-      </div>
+    <div className="flex flex-col items-center align-middle justify-center min-h-screen text-center bg-gray-100">
+      <Image src={Logo} alt="logo" width={500} height={500}></Image>
+      <h1 className="text-4xl font-bold mb-4">QR Code Expired</h1>
+      <p className="text-xl mb-8">
+        Your ordering session has ended. We appreciate your visit—thank you!
+      </p>
     </div>
   );
 }
